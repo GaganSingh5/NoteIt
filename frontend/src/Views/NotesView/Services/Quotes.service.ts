0c0bf2fa7a4ce9b25212efbcc777e0acadac9290
Gagandeep Singh <gagansingh0797@gmail.com>
2022-07-25 01:31:39 +0530

    updated URL for quotes api

--- frontend/src/Views/NotesView/Services/Quotes.service.ts
+++ frontend/src/Views/NotesView/Services/Quotes.service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 // import qs from 'qs';
 
 
-const getQuoteURL = "http://localhost:5000/api/quote/today";
+const getQuoteURL = `${process.env.REACT_APP_BACKEND_URL}${process.env.REACT_APP_QUOTE_URI}`;
 
 const getQuoteHandler = () => {
     return axios.get(getQuoteURL);
